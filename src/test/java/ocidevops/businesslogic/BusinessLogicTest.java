94a114d913b5bf66ef5a6504c945f6e789856cfe
Senthil Rajendran <86869195+skrajend@users.noreply.github.com>
2021-07-23 16:40:40 +0530

    Update BusinessLogicTest.java

--- src/test/java/ocidevops/businesslogic/BusinessLogicTest.java
+++ src/test/java/ocidevops/businesslogic/BusinessLogicTest.java
@@ -13,9 +13,21 @@ public class BusinessLogicTest {
 	}
 	
 	@Test
-	public void addAllTest() {
+	public void addAllTest1() {
 		BusinessLogic businessLogic = new BusinessLogic();
 		assertEquals(6, businessLogic.addAll(1,2,3));
+	}
+	
+	@Test
+	public void addAllTest2() {
+		BusinessLogic businessLogic = new BusinessLogic();
+		assertEquals(10, businessLogic.addAll(4,5,1));
+	}
+	
+	@Test
+	public void addAllTest3() {
+		BusinessLogic businessLogic = new BusinessLogic();
+		assertEquals(5, businessLogic.addAll(3,1,2));
 	}
 	
 }
